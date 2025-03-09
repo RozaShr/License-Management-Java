fe180e0e3fe4649b47a7561012b5b069e88372a1
Roza Shrestha <141385301+RozaShr@users.noreply.github.com>
2025-03-09 19:22:23 +0545

    Add files via upload

---
+++ UserApplication.java
@@ -0,0 +1,44 @@
+package LicenseManagement;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class UserApplication {
+    private String applicantName;
+    private String licenseType;
+    private LocalDate applicationDate;
+    private LocalDate expiryDate;
+    private String applicationID;
+    private String applicationStatus;
+
+    public UserApplication(String applicantName, String licenseType) {
+        this.applicantName = applicantName;
+        this.licenseType = licenseType;
+        this.applicationID = UUID.randomUUID().toString().substring(0,8);
+        this. applicationDate = LocalDate.now();
+        this.expiryDate = applicationDate.plusYears(5);
+    }
+    public String getApplicationID(){
+        return applicationID;
+    }
+    public String getApplicantName() {
+        return applicantName;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public LocalDate getExpiryDate(){
+        return expiryDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Application ID: " + applicationID+ " | Applicant: " + applicantName + " | License Type: " + licenseType;
+    }
+}
